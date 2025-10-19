656251d225eb7f5b412d7352dd39ec436c1d9242
unknown <jpbenevideslima0707@gmail.com>
2025-10-19 00:31:18 -0300

    adding SpringApplication packages on datagateway

--- datagateway/src/main/java/com/example/datagateway/DatagatewayApplication.java
+++ datagateway/src/main/java/com/example/datagateway/DatagatewayApplication.java
@@ -2,12 +2,22 @@ package com.example.datagateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+    "com.example.datagateway",
+    "com.example.common"
+})
+@EntityScan(basePackages = {
+    "com.example.common.models"
+})
+@EnableJpaRepositories(basePackages = {
+    "com.example.datagateway.repositories"
+})
 public class DatagatewayApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DatagatewayApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(DatagatewayApplication.class, args);
+    }
 }
+
