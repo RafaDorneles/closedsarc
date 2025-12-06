52533fd3f3258c46ca90ccb749a2bc6e9e47a07e
unknown <jpbenevideslima0707@gmail.com>
2025-12-06 14:46:31 -0300

    apigateway working

--- apigateway/src/main/java/com/example/apigateway/ApigatewayApplication.java
+++ apigateway/src/main/java/com/example/apigateway/ApigatewayApplication.java
@@ -2,10 +2,11 @@ package com.example.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ApigatewayApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ApigatewayApplication.class, args);
 	}
